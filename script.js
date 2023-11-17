a36411ba4888ba668566ecc98be11172bddbaeb5
Jay Singh <learn.jayssingh@gmail.com>
2023-11-17 23:49:25 +0530

    Project Completed, Summary of c hanges: > HTML => > Made a div for display and put the paragraph inside it > updated the credits link to repo instead of profile > CSS => > Added Font-face (Added source links for the same in /Assets) > Added styles of newly created digits paragraph > KeyPad styling changed to grey from complete red > right bottom corner aligned text in display > SCRIPT => > Executed the algebraic expressions (went kinda easy ;-;)

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+let currentInput = '';
+let storedInput = '';
+let operator = null;
 
+const displayScreen = document.querySelector('.digits');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        currentInput += button.innerText;
+        displayScreen.innerText = currentInput;
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        switch (button.id) {
+            case 'clear':
+                currentInput = '';
+                storedInput = '';
+                operator = null;
+                displayScreen.innerText = '';
+                break;
+            case 'sign':
+                currentInput = currentInput.charAt(0) === '-' ? currentInput.slice(1) : '-' + currentInput;
+                displayScreen.innerText = currentInput;
+                break;
+            case 'percent':
+                currentInput = parseFloat(currentInput) / 100;
+                displayScreen.innerText = currentInput;
+                break;
+            default:
+                if (operator !== null) return;
+                operator = button.innerText;
+                storedInput = currentInput;
+                currentInput = '';
+        }
+    });
+});
+
+document.querySelector('#equals').addEventListener('click', () => {
+    if (operator === null || storedInput === '') return;
+    let result = eval(storedInput + operator + currentInput);
+    displayScreen.innerText = result;
+    currentInput = result;
+    operator = null;
+    storedInput = '';
+});
